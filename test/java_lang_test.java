004ff730259d6df12a4fe1595e366f5915f5c950
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-02-18 13:02:57 -0500

    Adding Java and Rust language tests

---
+++ test/java_lang_test.java
@@ -0,0 +1,7 @@
+#!/usr/bin/env car
+
+public class java_lang_test {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!\nThis is a Java source file with the file extension '.java'.");
+  }
+}
---
+++ test/rust_lang_test.rs
@@ -0,0 +1,5 @@
+#!/usr/bin/env car
+
+fn main() {
+    println!("Hello, world!\nThis is a Rust source file with the file extension '.rs'.")
+}
