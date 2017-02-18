ecbd959f1919e796fe66fed39f8493c7d4e6d4cc
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-02-18 11:31:43 -0500

    Adding a bit of spiffy error messaging

--- src/car.sh
+++ src/car.sh
@@ -1,5 +1,68 @@
 #!/usr/bin/env bash
 
+if test -t 1; then
+  ncolors=$(tput colors)
+
+  if test -n "$ncolors" && test $ncolors -ge 16; then
+    color_default="$(tput sgr0)"
+    color_black="$(tput setaf 0)"
+    color_red="$(tput setaf 1)"
+    color_green="$(tput setaf 2)"
+    color_yellow="$(tput setaf 3)"
+    color_blue="$(tput setaf 4)"
+    color_magenta="$(tput setaf 5)"
+    color_cyan="$(tput setaf 6)"
+    color_light_gray="$(tput setaf 7)"
+    color_gray="$(tput setaf 8)"
+    color_light_red="$(tput setaf 9)"
+    color_light_green="$(tput setaf 10)"
+    color_light_yellow="$(tput setaf 11)"
+    color_light_blue="$(tput setaf 12)"
+    color_light_magenta="$(tput setaf 13)"
+    color_light_cyan="$(tput setaf 14)"
+    color_white="$(tput setaf 15)"
+  fi
+fi
+
+function IsColor
+{
+  ret=false
+  if [ -n "$1" ]; then
+    if [ "$1" == "$color_default" ] ||
+       [ "$1" == "$color_black" ] ||
+       [ "$1" == "$color_red" ] ||
+       [ "$1" == "$color_green" ] ||
+       [ "$1" == "$color_yellow" ] ||
+       [ "$1" == "$color_blue" ] ||
+       [ "$1" == "$color_magenta" ] ||
+       [ "$1" == "$color_cyan" ] ||
+       [ "$1" == "$color_light_gray" ] ||
+       [ "$1" == "$color_gray" ] ||
+       [ "$1" == "$color_light_red" ] ||
+       [ "$1" == "$color_light_green" ] ||
+       [ "$1" == "$color_light_yellow" ] ||
+       [ "$1" == "$color_light_blue" ] ||
+       [ "$1" == "$color_light_magenta" ] ||
+       [ "$1" == "$color_light_cyan" ] ||
+       [ "$1" == "$color_white" ]
+    then
+      ret=true
+    fi
+  fi
+  echo $ret
+}
+
+function ColorEcho
+{
+  if [ -z "$1" ]; then
+    echo -e "ColorEcho: Invalid color <null> provided for first argument." >&2
+  elif [ $(IsColor "$1") != true ]; then
+    echo -e "ColorEcho: Invalid color '$1' provided for first argument." >&2
+  else
+    echo -e "$1$2$color_default"
+  fi
+}
+
 SRC_FILE="$1"
 TEMP_OUT_FILE=$(mktemp -q)
 
@@ -13,7 +76,26 @@ function error()
 {
   [ -n "${ORIGINAL_SRC_FILE_CONTENTS}" ] && echo "${ORIGINAL_SRC_FILE_CONTENTS}" > "${SRC_FILE}"
   [ -f "${TEMP_OUT_FILE}" ] && rm "${TEMP_OUT_FILE}"
-  exit $1
+
+  ERROR_MESSAGE="$2"
+  if [ -z "$1" ] || [ "$(isNumber $1)" == "true" ]; then
+    EXIT_CODE=$1
+  else
+    EXIT_CODE=255
+    ERROR_MESSAGE="$1"
+  fi
+
+  [ -n "$ERROR_MESSAGE" ] && echo "car: $(ColorEcho $color_red error): ${ERROR_MESSAGE}"
+  exit $EXIT_CODE
+}
+
+function isNumber()
+{
+  if [ -n "$1" ] && [[ $1 =~ ^[0-9]+$ ]]; then
+    echo true
+  else
+    echo false
+  fi
 }
 
 ([ -n "${SRC_FILE}" ] && [ -f "${SRC_FILE}" ]) || usage 1
